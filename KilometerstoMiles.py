ba2f3c77db5a82a8b38cd1cc985c5fc5e5dee090
Keith Dsouza <75474616+KeithDsouza25@users.noreply.github.com>
2021-10-14 11:44:13 +0530

    Create KilometerstoMiles.py

---
+++ KilometerstoMiles.py
@@ -0,0 +1,5 @@
+kilometre_1 = float (input ("Please enter the distance travelled by car in Kilometres as a unit: "))  
+conversion_ratio_1 = 0.621371  
+miles_1 = kilometre_1 * conversion_ratio_1  
+print ("The speed value of car in Miles: ", miles_1)  
+ 
