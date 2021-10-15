ba1b97c4eabb40b863f7f8e5945a96dc3126cc6d
Keith Dsouza <75474616+KeithDsouza25@users.noreply.github.com>
2021-10-15 10:21:16 +0530

    Create Swapping2Numbers.py

---
+++ Swapping2Numbers.py
@@ -0,0 +1,8 @@
+A = int( input("Please enter value for A: "))  
+B = int( input("Please enter value for B: "))  
+   
+# To Swap the values of two variables  
+A, B = B, A  
+   
+print ("The Value of P after swapping: ", A)  
+print ("The Value of Q after swapping: ", B)  
